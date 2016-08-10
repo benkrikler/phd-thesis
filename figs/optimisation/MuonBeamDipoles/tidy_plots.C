a845b1d6b81ff4b2898b17c965f7d226b9af6441
Ben Krikler <bek07@ic.ac.uk>
2016-08-10 14:03:22 +0100

    Resume work on the optimisation chapter

---
+++ figs/optimisation/MuonBeamDipoles/tidy_plots.C
@@ -0,0 +1,37 @@
+void tidy_plots(const char* ext="png"){
+
+  gROOT->ProcessLine(".L ~/thesis/figs/tidy_plot_tools.C+");
+  gStyle->SetPalette(55);
+  gStyle->SetPaintTextFormat("0.3e");
+//  gStyle->SetPadRightMargin(0.5);
+  gStyle->SetTextColor(kWhite);
+
+  const TString top_dir="~/comet/1601w03_PhII_MuBeamOptimisation/dipole_optimisation_analysis/";
+  PlotConfig config;
+
+  ///////////////////////   Stopping Rate
+  config.reset();
+  config.y_axis_label_offset = 1.00;
+  config.y_axis_label    = "Torus 2 Scale Factor ( #times 0.055 T)";
+  config.x_axis_label    = "Torus 1 Scale Factor ( #times 0.055 T)";
+  config.y_axis_decimal  = true;
+  config.x_axis_decimal  = true;
+  config.x_axis_range_high = 1.3;
+  config.x_axis_range_low  = 0.7;
+  config.y_axis_range_high = 1.3;
+  config.y_axis_range_low  = 0;
+  //config.shift_plot_x  = -0.03;
+  config.shift_palette  = -0.04;
+  config.canvas_ratio=1.1;
+  config.pad_margin_right= 0.14;
+  config.pad_margin_left= 0.08;
+  config.stats_force_off  = true;
+  config.force_draw_option  = "colzttext";
+  config.marker_color=kWhite;
+
+  file=FixPlot(top_dir+"stopped_muons.root","", config);
+  SavePlot(ext);
+
+  file=FixPlot(top_dir+"stopped_pions.root","", config);
+  SavePlot(ext);
+}
