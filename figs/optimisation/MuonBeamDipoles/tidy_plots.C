1ed78aff3228c6be6846f43ac1fc12e280cdc7bc
Ben Krikler <bek07@ic.ac.uk>
2016-08-12 07:37:24 +0100

    Add titles to plots for stopped mu / pi rate vs. dipole field

--- figs/optimisation/MuonBeamDipoles/tidy_plots.C
+++ figs/optimisation/MuonBeamDipoles/tidy_plots.C
@@ -1,6 +1,8 @@
 void tidy_plots(const char* ext="png"){
 
   gROOT->ProcessLine(".L ~/thesis/figs/tidy_plot_tools.C+");
+  TGaxis::SetMaxDigits(3);
+   gStyle->SetStripDecimals(false);
   gStyle->SetPalette(55);
   gStyle->SetPaintTextFormat("0.3e");
 //  gStyle->SetPadRightMargin(0.5);
@@ -14,6 +16,7 @@ void tidy_plots(const char* ext="png"){
   config.y_axis_label_offset = 1.00;
   config.y_axis_label    = "Torus 2 Scale Factor ( #times 0.055 T)";
   config.x_axis_label    = "Torus 1 Scale Factor ( #times 0.055 T)";
+  config.z_axis_label_centred= true;
   config.y_axis_decimal  = true;
   config.x_axis_decimal  = true;
   config.x_axis_range_high = 1.3;
@@ -29,9 +32,11 @@ void tidy_plots(const char* ext="png"){
   config.force_draw_option  = "colzttext";
   config.marker_color=kWhite;
 
+  config.z_axis_label    = "Muon Stops per POT";
   file=FixPlot(top_dir+"stopped_muons.root","", config);
   SavePlot(ext);
 
+  config.z_axis_label    = "Pion Stops per POT";
   file=FixPlot(top_dir+"stopped_pions.root","", config);
   SavePlot(ext);
 }
