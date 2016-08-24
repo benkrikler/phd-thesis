2d2760be1883fe055d180428ac88d7754fb02071
Ben Krikler <bek07@ic.ac.uk>
2016-08-24 17:33:17 +0100

    Plots for geometric acceptance of signal

--- figs/sensitivity/tidy_plots.C
+++ figs/sensitivity/tidy_plots.C
@@ -23,74 +23,73 @@ void tidy_plots(const char* ext="png"){
   AnnotatedLine        EST_start("", 21800, plot::kMin, 21800, plot::kMax); EST_start.SetLineAppearance       (kMagenta, 2 ,2);
   AnnotatedLine     DetSol_start("", 28000, plot::kMin, 28000, plot::kMax); DetSol_start.SetLineAppearance    (kMagenta, 2 ,2);
 
-  //====================================================
-  config.y_axis_label_offset = 1.35;
-  config.y_axis_decimal  = true;
-  //config.x_axis_range_high  = 33e3;
-  //config.y_axis_range_high  = 110;
-  //config.y_axis_label  = "Observed Momentum (MeV/c)";
-  //config.x_axis_label  = "Initial Momentum (MeV/c)";
-  config.log_y=false; config.log_z=false;
-  config.stats_force_off=true;
-  config.legend_remove=true;
-  config.grid_y=config.grid_x=true;
-  config.force_draw_option="colz";
-  config.canvas_ratio=1.0001;
-  config.normalise=1119377985.;
-  config.title  = " ";
-
-  top_dir="~/comet/160807_BeamBackgrounds/160824_plots/G4Trajectories_IStoppedInTgtAnalyzer/";
-
-  // === 2D plots ===
-  config.z_axis_label="#mu stops per POT per {x_bin_widths} #times{y_bin_widths} mm^{2}";
-  config.z_axis_label_centred=true;
-  config.z_axis_label_offset=1.4;
-  config.shift_plot_x=-0.03;
-
-  config.y_axis_range_low=-(config.y_axis_range_high=110);
-  file=FixPlot(top_dir+"hPosition3D/hPosition3D_13XY_lin.root","TH1", config);
-  SavePlot(ext,NULL,"MuStops_2D_XY");
-
-  config.rebin_x=5;
-  config.y_axis_range_low=-(config.y_axis_range_high=110);
-  config.x_axis_range_low=1000; config.x_axis_range_high=1900;
-  file=FixPlot(top_dir+"hPosition3D/hPosition3D_13ZY_log.root","TH1", config);
-  SavePlot(ext,NULL,"MuStops_2D_ZY");
-
-  config.y_axis_label_offset = 1.55;
-  config.rebin_y=config.rebin_x;
-  config.rebin_x=1;
-  //config.y_axis_range_low=-(config.y_axis_range_high=110);
-  config.x_axis_range_low=0; config.x_axis_range_high=-1;
-  config.y_axis_range_low=1000; config.y_axis_range_high=1900;
-  file=FixPlot(top_dir+"hPosition3D/hPosition3D_13XZ_lin.root","TH1", config);
-  SavePlot(ext,NULL,"MuStops_2D_XZ");
-
-  //  === 1D plots ===
-  config.shift_plot_x=-0.05;
-  config.y_axis_label_offset = 1.35;
-  config.y_axis_range_low=-(config.y_axis_range_high=0);
-  config.y_axis_label="Muon Stopping Rate per POT per {x_bin_widths} mm";
-  config.rebin_x=1;
-  config.shift_plot_x=0;
-
-  //config.x_axis_range_low=-(config.x_axis_range_high=110);
-  file=FixPlot(top_dir+"hPositionX/hPositionX_13_lin.root","TH1", config);
-  SavePlot(ext,NULL,"MuStops_1D_X");
-
-  config.x_axis_range_low=-(config.x_axis_range_high=110);
-  file=FixPlot(top_dir+"hPositionY/hPositionY_13_lin.root","TH1", config);
-  SavePlot(ext,NULL,"MuStops_1D_Y");
-
-  config.y_axis_label_offset = 1.55;
-  config.rebin_x=5;
-  config.x_axis_range_low=1000; config.x_axis_range_high=1900;
-  file=FixPlot(top_dir+"hPositionZ/hPositionZ_13_lin.root","TH1", config);
-  SavePlot(ext,NULL,"MuStops_1D_Z");
-
-  return;
+//  //====================================================
+//  config.y_axis_label_offset = 1.35;
+//  config.y_axis_decimal  = true;
+//  //config.x_axis_range_high  = 33e3;
+//  //config.y_axis_range_high  = 110;
+//  //config.y_axis_label  = "Observed Momentum (MeV/c)";
+//  //config.x_axis_label  = "Initial Momentum (MeV/c)";
+//  config.log_y=false; config.log_z=false;
+//  config.stats_force_off=true;
+//  config.legend_remove=true;
+//  config.grid_y=config.grid_x=true;
+//  config.force_draw_option="colz";
+//  config.canvas_ratio=1.0001;
+//  config.normalise=1119377985.;
+//  config.title  = " ";
+//
+//  top_dir="~/comet/160807_BeamBackgrounds/160824_plots/G4Trajectories_IStoppedInTgtAnalyzer/";
+//
+//  // === 2D plots ===
+//  config.z_axis_label="#mu stops per POT per {x_bin_widths} #times{y_bin_widths} mm^{2}";
+//  config.z_axis_label_centred=true;
+//  config.z_axis_label_offset=1.4;
+//  config.shift_plot_x=-0.03;
+//
+//  config.y_axis_range_low=-(config.y_axis_range_high=110);
+//  file=FixPlot(top_dir+"hPosition3D/hPosition3D_13XY_lin.root","TH1", config);
+//  SavePlot(ext,NULL,"MuStops_2D_XY");
+//
+//  config.rebin_x=5;
+//  config.y_axis_range_low=-(config.y_axis_range_high=110);
+//  config.x_axis_range_low=1000; config.x_axis_range_high=1900;
+//  file=FixPlot(top_dir+"hPosition3D/hPosition3D_13ZY_log.root","TH1", config);
+//  SavePlot(ext,NULL,"MuStops_2D_ZY");
+//
+//  config.y_axis_label_offset = 1.55;
+//  config.rebin_y=config.rebin_x;
+//  config.rebin_x=1;
+//  //config.y_axis_range_low=-(config.y_axis_range_high=110);
+//  config.x_axis_range_low=0; config.x_axis_range_high=-1;
+//  config.y_axis_range_low=1000; config.y_axis_range_high=1900;
+//  file=FixPlot(top_dir+"hPosition3D/hPosition3D_13XZ_lin.root","TH1", config);
+//  SavePlot(ext,NULL,"MuStops_2D_XZ");
+//
+//  //  === 1D plots ===
+//  config.shift_plot_x=-0.05;
+//  config.y_axis_label_offset = 1.35;
+//  config.y_axis_range_low=-(config.y_axis_range_high=0);
+//  config.y_axis_label="Muon Stopping Rate per POT per {x_bin_widths} mm";
+//  config.rebin_x=1;
+//  config.shift_plot_x=0;
+//
+//  //config.x_axis_range_low=-(config.x_axis_range_high=110);
+//  file=FixPlot(top_dir+"hPositionX/hPositionX_13_lin.root","TH1", config);
+//  SavePlot(ext,NULL,"MuStops_1D_X");
+//
+//  config.x_axis_range_low=-(config.x_axis_range_high=110);
+//  file=FixPlot(top_dir+"hPositionY/hPositionY_13_lin.root","TH1", config);
+//  SavePlot(ext,NULL,"MuStops_1D_Y");
+//
+//  config.y_axis_label_offset = 1.55;
+//  config.rebin_x=5;
+//  config.x_axis_range_low=1000; config.x_axis_range_high=1900;
+//  file=FixPlot(top_dir+"hPositionZ/hPositionZ_13_lin.root","TH1", config);
+//  SavePlot(ext,NULL,"MuStops_1D_Z");
 
   //====================================================
+  config.reset()
   //config.y_axis_label_offset = 1.3;
   config.y_axis_decimal  = true;
   config.x_axis_range_high  = 33e3;
@@ -101,6 +100,7 @@ void tidy_plots(const char* ext="png"){
   config.stats_force_off=true;
   config.grid_y=config.grid_x=true;
   config.force_draw_option="col";
+  config.canvas_grow=3;
 
   top_dir="~/comet/1603w12_Phase-II_estimates/160328_signal_acceptance/";
   file=FixPlot(top_dir+"160417_plot_SignalElectronAcceptanceVsBeamline.root","TH1", config);
@@ -108,7 +108,7 @@ void tidy_plots(const char* ext="png"){
 
   file=FixPlot(top_dir+"160417_plot_SignalElectron_HeightVsBeamline.root","TH1", config);
   if(geomHist) Overlay(geomHist,"same",config);
-  SavePlot(ext,NULL,"Tidied_SignalHeight2DVsBeamline");
+  SavePlot("png",NULL,"Tidied_SignalHeight2DVsBeamline");
 
   return;
 
